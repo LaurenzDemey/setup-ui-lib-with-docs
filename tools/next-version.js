fe45463816c29323e7b748a3a2a6290e5e677ac0
Laurenz Demey (EXT) <laurenz.demey.ext@mobilit.fgov.be>
2024-02-01 16:47:34 +0100

    CI: changes to process

---
+++ tools/next-version.js
@@ -0,0 +1,20 @@
+/**
+ * This file is used in a github action, to identify the next version.
+ * This is normally not needed to release our libraries, but because we want to version our docs as well using the same number.
+ * I use this to first calculate it & store it for later usage.
+ */
+
+const { releaseVersion } = require('nx/release');
+
+(async () => {
+  try {
+    const { workspaceVersion } = await releaseVersion({
+      verbose: false,
+      dryRun: true,
+    });
+    console.log(`::set-output name=next-version::${workspaceVersion}`);
+  } catch (error) {
+    console.error('Error occurred:', error.message);
+    process.exit(1); // Exit with a non-zero status code to indicate failure
+  }
+})();
